384c8bc1a773c1dc4a2f946e5ce3dce39d515208
leorenis <leorenis@gmail.com>
2019-06-10 07:02:16 -0300

    Add context on app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,30 @@
 import React from 'react'
+import UserCreate from '../UserCreate'
 
-const App = () => <div> Hello App</div>
+class App extends React.Component {
+  state = { language: 'english' }
 
+  onLanguageChange = language => {
+    this.setState({ language })
+  }
+
+  render() {
+    return (
+      <div className={'ui container'}>
+        <div>
+          Select a language:
+          <i
+            className={'flag us'}
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className={'flag br'}
+            onClick={() => this.onLanguageChange('portuguese')}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    )
+  }
+}
 export default App
---
+++ src/components/Field/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import LanguageContext from '../../contexts/LanguageContext'
+
+class Field extends React.Component {
+  static contextType = LanguageContext
+  render() {
+    return (
+      <div className={'ui field'}>
+        <label>Name</label>
+        <input />
+      </div>
+    )
+  }
+}
+export default Field
